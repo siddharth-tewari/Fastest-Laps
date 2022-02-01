9604fc1c42123920c3fa820e0bf128f1fc9fcb39
GitHub Action <action@github.com>
2022-02-01 17:13:47 +0000

    update table data

---
+++ f2standingData.js
@@ -0,0 +1,10 @@
+var tabledata=`<tbody>
+	<td colspan='100' style='text-align:center'>The 2022 season is not currently in session</td>
+</tbody>
+`;
+document.getElementById('f2-drivers').innerHTML+=tabledata;
+var tabledata=`<tbody>
+	<td colspan='100' style='text-align:center'>The 2022 season is not currently in session</td>
+</tbody>
+`;
+document.getElementById('f2-constructors').innerHTML+=tabledata;
