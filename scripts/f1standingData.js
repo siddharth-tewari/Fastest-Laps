a96b093c842a9e5b00f20918d84e1546e7ce8483
Siddharth Tewari <76590952+siddharth-tewari@users.noreply.github.com>
2023-02-14 09:24:44 +0530

    Add files via upload

---
+++ scripts/f1standingData.js
@@ -0,0 +1,11 @@
+var tabledata=`<tbody>
+	<td colspan='100' style='text-align:center'>The 2023 season is not currently in session</td>
+</tbody>
+`;
+document.getElementById('f1-drivers').innerHTML+=tabledata;
+var tabledata=`<tbody>
+	<td colspan='100' style='text-align:center'>The 2023 season is not currently in session</td>
+</tbody>
+`;
+document.getElementById('f1-constructors').innerHTML+=tabledata;
+document.getElementById('lastup').innerHTML='13/02/2023 23:31:21 UTC';
