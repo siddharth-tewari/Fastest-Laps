f0c41be80031f8d13fdb72b1442d22ecc3d58919
GitHub Action <action@github.com>
2023-08-28 00:35:55 +0000

    update table data

--- scripts/f1standingData.js
+++ scripts/f1standingData.js
@@ -192,4 +192,4 @@ var tabledata=`<table border="1" class="dataframe">
 </table>
 `;
 document.getElementById('f1-constructors').innerHTML+=tabledata;
-document.getElementById('lastup').innerHTML='27/08/2023 23:32:56 UTC';
+document.getElementById('lastup').innerHTML='28/08/2023 00:35:45 UTC';
