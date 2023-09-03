0c9c9f0524613a924669365104f22f8b0f154c30
GitHub Action <action@github.com>
2023-09-03 15:07:26 +0000

    update table data

--- scripts/f1standingData.js
+++ scripts/f1standingData.js
@@ -4,49 +4,49 @@ var tabledata=`<table border="1" class="dataframe">
       <td>1</td>
       <td>Max Verstappen VER</td>
       <td>Red Bull Racing Honda RBPT</td>
-      <td>339</td>
+      <td>364</td>
     </tr>
     <tr>
       <td>2</td>
       <td>Sergio Perez PER</td>
       <td>Red Bull Racing Honda RBPT</td>
-      <td>201</td>
+      <td>219</td>
     </tr>
     <tr>
       <td>3</td>
       <td>Fernando Alonso ALO</td>
       <td>Aston Martin Aramco Mercedes</td>
-      <td>168</td>
+      <td>170</td>
     </tr>
     <tr>
       <td>4</td>
       <td>Lewis Hamilton HAM</td>
       <td>Mercedes</td>
-      <td>156</td>
+      <td>164</td>
     </tr>
     <tr>
       <td>5</td>
       <td>Carlos Sainz SAI</td>
       <td>Ferrari</td>
-      <td>102</td>
+      <td>117</td>
     </tr>
     <tr>
       <td>6</td>
       <td>Charles Leclerc LEC</td>
       <td>Ferrari</td>
-      <td>99</td>
+      <td>111</td>
     </tr>
     <tr>
       <td>7</td>
       <td>George Russell RUS</td>
       <td>Mercedes</td>
-      <td>99</td>
+      <td>109</td>
     </tr>
     <tr>
       <td>8</td>
       <td>Lando Norris NOR</td>
       <td>McLaren Mercedes</td>
-      <td>75</td>
+      <td>79</td>
     </tr>
     <tr>
       <td>9</td>
@@ -62,21 +62,21 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>11</td>
-      <td>Esteban Ocon OCO</td>
-      <td>Alpine Renault</td>
+      <td>Oscar Piastri PIA</td>
+      <td>McLaren Mercedes</td>
       <td>36</td>
     </tr>
     <tr>
       <td>12</td>
-      <td>Oscar Piastri PIA</td>
-      <td>McLaren Mercedes</td>
+      <td>Esteban Ocon OCO</td>
+      <td>Alpine Renault</td>
       <td>36</td>
     </tr>
     <tr>
       <td>13</td>
       <td>Alexander Albon ALB</td>
       <td>Williams Mercedes</td>
-      <td>15</td>
+      <td>21</td>
     </tr>
     <tr>
       <td>14</td>
@@ -88,7 +88,7 @@ var tabledata=`<table border="1" class="dataframe">
       <td>15</td>
       <td>Valtteri Bottas BOT</td>
       <td>Alfa Romeo Ferrari</td>
-      <td>5</td>
+      <td>6</td>
     </tr>
     <tr>
       <td>16</td>
@@ -122,13 +122,13 @@ var tabledata=`<table border="1" class="dataframe">
     </tr>
     <tr>
       <td>21</td>
-      <td>Daniel Ricciardo RIC</td>
+      <td>Liam Lawson LAW</td>
       <td>AlphaTauri Honda RBPT</td>
       <td>0</td>
     </tr>
     <tr>
       <td>22</td>
-      <td>Liam Lawson LAW</td>
+      <td>Daniel Ricciardo RIC</td>
       <td>AlphaTauri Honda RBPT</td>
       <td>0</td>
     </tr>
@@ -141,27 +141,27 @@ var tabledata=`<table border="1" class="dataframe">
     <tr>
       <td>1</td>
       <td>Red Bull Racing Honda RBPT</td>
-      <td>540</td>
+      <td>583</td>
     </tr>
     <tr>
       <td>2</td>
       <td>Mercedes</td>
-      <td>255</td>
+      <td>273</td>
     </tr>
     <tr>
       <td>3</td>
-      <td>Aston Martin Aramco Mercedes</td>
-      <td>215</td>
+      <td>Ferrari</td>
+      <td>228</td>
     </tr>
     <tr>
       <td>4</td>
-      <td>Ferrari</td>
-      <td>201</td>
+      <td>Aston Martin Aramco Mercedes</td>
+      <td>217</td>
     </tr>
     <tr>
       <td>5</td>
       <td>McLaren Mercedes</td>
-      <td>111</td>
+      <td>115</td>
     </tr>
     <tr>
       <td>6</td>
@@ -171,7 +171,7 @@ var tabledata=`<table border="1" class="dataframe">
     <tr>
       <td>7</td>
       <td>Williams Mercedes</td>
-      <td>15</td>
+      <td>21</td>
     </tr>
     <tr>
       <td>8</td>
@@ -181,7 +181,7 @@ var tabledata=`<table border="1" class="dataframe">
     <tr>
       <td>9</td>
       <td>Alfa Romeo Ferrari</td>
-      <td>9</td>
+      <td>10</td>
     </tr>
     <tr>
       <td>10</td>
@@ -192,4 +192,4 @@ var tabledata=`<table border="1" class="dataframe">
 </table>
 `;
 document.getElementById('f1-constructors').innerHTML+=tabledata;
-document.getElementById('lastup').innerHTML='03/09/2023 14:32:50 UTC';
+document.getElementById('lastup').innerHTML='03/09/2023 15:07:18 UTC';
